e7f2820e91b164e06662b443a65bfc87021f30e3
Jaison Robson Gusava <jaison.gusava@gett.com.br>
2018-07-17 17:28:09 -0300

    refactor(common): criado TodoTable, Todo(Text|Check|Delete) use Consumer

--- src/TodoText.js
+++ src/TodoText.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import { TodosConsumer } from './TodosContext'
 
 const getBackgroundColor = ({ typing }) => {
   if (typing) {
@@ -14,54 +15,59 @@ class TodoText extends Component {
   }
 
   render() {
-    const { changeTodo, todo } = this.props
+    const { todo } = this.props
     const { editing } = this.state
     return (
-      <Fragment>
-        <span
-          style={{
-            visibility: editing ? 'hidden' : 'visible',
-            position: editing ? 'absolute' : 'relative',
-            backgroundColor: todo.isCompleted ? 'gray' : '',
-            textDecoration: todo.isCompleted ? 'line-through' : '',
-          }}
-          onDoubleClick={() => {
-            this.setState({ editing: true })
-          }}
-        >
-          {todo.name}
-        </span>
-        <input
-          style={{
-            backgroundColor: getBackgroundColor({
-              ...this.state,
-              todo,
-            }),
-            visibility: editing ? 'visible' : 'hidden',
-            position: editing ? 'relative' : 'absolute',
-            width: '200px',
-          }}
-          defaultValue={todo.name}
-          onKeyDown={(event) => {
-            const newName = event.target.value
-            if (event.key === 'Enter') {
-              const payload = {
-                ...todo,
-                name: newName,
-              }
-              changeTodo(todo, payload)
-              this.setState({ typing: false })
-              this.setState({ editing: false })
-            }
-          }}
-          onKeyUp={(event) => {
-            const newName = event.target.value
-            this.setState({
-              typing: newName !== todo.name,
-            })
-          }}
-        />
-      </Fragment>
+      <TodosConsumer>
+        {({
+          changeTodo,
+        }) => (
+          <Fragment>
+            <span
+              style={{
+                visibility: editing ? 'hidden' : 'visible',
+                position: editing ? 'absolute' : 'relative',
+                backgroundColor: todo.isCompleted ? 'gray' : '',
+                textDecoration: todo.isCompleted ? 'line-through' : '',
+              }}
+              onDoubleClick={() => {
+                this.setState({ editing: true })
+              }}
+            >
+              {todo.name}
+            </span>
+            <input
+              style={{
+                backgroundColor: getBackgroundColor({
+                  ...this.state,
+                  todo,
+                }),
+                visibility: editing ? 'visible' : 'hidden',
+                position: editing ? 'relative' : 'absolute',
+                width: '200px',
+              }}
+              defaultValue={todo.name}
+              onKeyDown={(event) => {
+                const newName = event.target.value
+                if (event.key === 'Enter') {
+                  const payload = {
+                    ...todo,
+                    name: newName,
+                  }
+                  changeTodo(todo, payload)
+                  this.setState({ typing: false, editing: false })
+                }
+              }}
+              onKeyUp={(event) => {
+                const newName = event.target.value
+                this.setState({
+                  typing: newName !== todo.name,
+                })
+              }}
+            />
+          </Fragment>
+        )}
+      </TodosConsumer>
     )
   }
 }
--- src/Todolist.js
+++ src/Todolist.js
@@ -19,41 +19,89 @@ const getFilteredTodos = ({ todolist, filterBy }) => (
   todolist.filter(toFilter(filterBy))
 )
 
-const TodoCheck = ({ todo, changeTodo }) => (
-  <ButtonGroup>
-    <Button
-      color={todo.isCompleted ? "secondary" : "primary"}
-      id={todo.id}
-      active={todo.isCompleted}
-      style={{ width: 35, height: 35, color: 'white' }}
-      onClick={
-        () => {
-          const payload = {
-            ...todo,
-            isCompleted: !todo.isCompleted,
-          }
-          changeTodo(todo, payload)
-        }}
-    >
-      {todo.isCompleted ? '✔' : ''}
-    </Button>
-  </ButtonGroup>
+const TodoCheck = ({ todo }) => (
+  <TodosConsumer>
+    {({
+      changeTodo,
+    }) => (
+      <ButtonGroup>
+        <Button
+          color={todo.isCompleted ? "secondary" : "primary"}
+          id={todo.id}
+          active={todo.isCompleted}
+          style={{ width: 35, height: 35, color: 'white' }}
+          onClick={
+            () => {
+              const payload = {
+                ...todo,
+                isCompleted: !todo.isCompleted,
+              }
+              changeTodo(todo, payload)
+            }}
+        >
+          {todo.isCompleted ? '✔' : ''}
+        </Button>
+      </ButtonGroup>
+    )}
+  </TodosConsumer>
 )
 
-const TodoDelete = ({ todo, onDelete }) => (
-  <button
-    type="button"
-    onClick={() => onDelete(todo)}
-    style={{
-      width: 60,
-      height: 35,
-      backgroundColor: 'Transparent',
-      backgroundRepeat: 'no-repeat',
-      border: 'none',
-    }}
-  >
-    <img src={require("./delete.png")} style={{ width: 15, height: 15 }} alt="" />
-  </button>
+const TodoDelete = ({ todo }) => (
+  <TodosConsumer>
+    {({
+      deleteTodo,
+    }) => (
+      <button
+        type="button"
+        onClick={() => deleteTodo(todo)}
+        style={{
+          width: 60,
+          height: 35,
+          backgroundColor: 'Transparent',
+          backgroundRepeat: 'no-repeat',
+          border: 'none',
+        }}
+      >
+        <img src={require("./delete.png")} style={{ width: 15, height: 15 }} alt="" />
+      </button>
+    )}
+  </TodosConsumer>
+)
+
+const TodoTable = ({ todos }) => (
+  <table>
+    <thead>
+      <tr>
+        <th style={{ width: '49%', textAlign: 'center' }}>
+          <Order name="completed">
+            Completada
+          </Order>
+        </th>
+        <th style={{ width: '49%', textAlign: 'center' }}>
+          <Order name="name">
+            Nome
+          </Order>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      {todos.map((todo) => {
+        return (
+          <tr key={todo.id}>
+            <td>
+              <TodoCheck todo={todo} />
+            </td>
+            <td>
+              <TodoText todo={todo} />
+            </td>
+            <td>
+              <TodoDelete todo={todo} />
+            </td>
+          </tr>
+        )
+      })}
+    </tbody>
+  </table>
 )
 
 const TodoList = () => (
@@ -64,57 +112,16 @@ const TodoList = () => (
           <TodosConsumer>
             {({
               todolist,
-              changeTodo,
-              deleteTodo: onDelete,
             }) => {
               const todolistFiltered = getFilteredTodos({ todolist, filterBy })
               const todolistOrdered = _.orderBy(todolistFiltered, ['isCompleted', 'name'], [orders.completed, orders.name])
-
               return (
-                <Fragment>
-                  {
-                    todolist.length > 0 ? (
-                      <div>
-                        <hr />
-                        <table>
-                          <thead>
-                            <tr>
-                              <th style={{ width: '49%', textAlign: 'center' }}>
-                                <Order name="completed">
-                                  Completada
-                                </Order>
-                              </th>
-                              <th style={{ width: '49%', textAlign: 'center' }}>
-                                <Order name="name">
-                                  Nome
-                                </Order>
-                              </th>
-                            </tr>
-                          </thead>
-                          {todolistOrdered.map((todo) => {
-                            return (
-                              <Fragment key={todo.id}>
-                                <tbody>
-                                  <tr>
-                                    <td>
-                                      <TodoCheck todo={todo} changeTodo={changeTodo} />
-                                    </td>
-                                    <td>
-                                      <TodoText changeTodo={changeTodo} todo={todo} />
-                                    </td>
-                                    <td>
-                                      <TodoDelete todo={todo} onDelete={onDelete} />
-                                    </td>
-                                  </tr>
-                                </tbody>
-                              </Fragment>
-                            )
-                          })}
-                        </table>
-                      </div>
-                    ) : ''
-                  }
-                </Fragment>
+                todolist.length > 0 ? (
+                  <div>
+                    <hr />
+                    <TodoTable todos={todolistOrdered} />
+                  </div>
+                ) : ''
               )
             }}
           </TodosConsumer>
