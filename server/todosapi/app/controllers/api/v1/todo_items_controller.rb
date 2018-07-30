bfc441c191ff73901b54a604166c395ee06d74ca
Jaison Robson Gusava <jaison.gusava@gett.com.br>
2018-07-30 16:32:51 -0300

    feat(common): add crud no backend

--- server/todosapi/app/controllers/api/v1/todo_items_controller.rb
+++ server/todosapi/app/controllers/api/v1/todo_items_controller.rb
@@ -6,6 +6,35 @@ module Api
         todos = TodoItem.order('created_at DESC');
         render json: {status: 'SUCCESS', message:'Todos carregados', data:todos},status: :ok
       end
+      def show
+        todo = TodoItem.find(params[:id])
+        render json: {status: 'SUCCESS', message:'Todo carregado', data:todo},status: :ok
+      end
+      def create
+        todo = TodoItem.new(todo_params)
+        if todo.save
+          render json: {status: 'SUCCESS', message: 'Todo criado', data: todo}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Todo nao salvo', data: todo.errors}, status: :unprocessable_entity
+        end
+      end
+      def destroy
+        todo = TodoItem.find(params[:id])
+        todo.destroy
+        render json: {status: 'SUCCESS', message: 'Todo deletado', data:todo}, status: :ok
+      end
+      def update
+        todo = TodoItem.find(params[:id])
+        if todo.update_attributes(todo_params)
+          render json: {status: 'SUCCESS', message: 'Todo atualizado', data:todo}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Todo nao atualizado', data:todo.errors}, status: :unprocessable_entity
+        end
+      end
+      private
+      def todo_params
+        params.require(:todo_item).permit(:name, :completed)
+      end
 		end
 	end
 end
--- server/todosapi/app/models/todo_item.rb
+++ server/todosapi/app/models/todo_item.rb
@@ -1,4 +1,4 @@
 class TodoItem < ApplicationRecord
   validates :name, presence: true
-  validates :completed, presence: true
+  validates :completed, :inclusion => { :in => [false, true]}
 end
