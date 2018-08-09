6a56ec2e55c1fa736a1b7d82bf5f53cecb52cf58
Jaison Robson Gusava <jaison.gusava@gett.com.br>
2018-08-09 16:06:08 -0300

    refact(server): else > found?

--- server/app/controllers/api/v1/todos_controller.rb
+++ server/app/controllers/api/v1/todos_controller.rb
@@ -1,20 +1,19 @@
 module Api
 	module V1
     class TodosController < ApplicationController
+
       def index
         todos = Todo.order('created_at DESC');
         render json: todos,status: :ok
       end
+
       def show
         todo = Todo.find_by(id: params[:id])
-        if todo.present?
+        if found?(todo)
           render json: todo, status: :ok
-        else
-          render json: {
-            error: "Todo with id #{params[:id]} not found."
-          }, status: :not_found
         end
       end
+
       def create
         todo = Todo.new(todo_params)
         if todo.save
@@ -23,32 +22,38 @@ module Api
           render json: todo.errors, status: :unprocessable_entity
         end
       end
+
       def destroy
         todo = Todo.find_by(id: params[:id])
-        if todo.present?
+        if found?(todo)
           todo.destroy
           render json: todo, status: :ok
-        else
-          render json: {
-            error: "Todo with id #{params[:id]} not found."
-          }, status: :not_found
         end
       end
+
       def update
         todo = Todo.find_by(id: params[:id])
-        if todo.present?
+        if found?(todo)
           if todo.update_attributes(todo_params)
             render json: todo, status: :ok
           else
             render json: todo.errors, status: :unprocessable_entity
           end
-        else
+        end
+      end
+
+      private
+
+      def found?(todo)
+        if todo.blank?
           render json: {
             error: "Todo with id #{params[:id]} not found."
           }, status: :not_found
         end
+
+        todo.present?
       end
-      private
+
       def todo_params
         params.require(:todo).permit(:name, :completed)
       end
