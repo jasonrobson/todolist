52e43820ac01e107e0606ec4f40d8edfff147f82
gtkatakura-bysoft <gabriel.takashi@gett.com.br>
2018-08-15 16:09:47 -0300

    refactor(todos): trocado arrow functions por class fields

--- client/src/TodoTextInput.js
+++ client/src/TodoTextInput.js
@@ -1,73 +1,72 @@
 import React, { Component, Fragment } from 'react'
+import { Compose } from 'react-powerplug'
 import { TodosConsumer } from './TodosContext'
 import { TodoConsumer } from './TodoContext'
 import AlertPopup from './AlertPopup'
-
-const getBackgroundColor = (typing) => {
-  if (typing) {
-    return 'red'
-  }
-  return 'white'
-}
 
 class TodoTextInput extends Component {
   state = {
     errorMessage: null,
   }
 
+  onKeyDown = async (event) => {
+    try {
+      const { todo } = this.props
+      const newName = event.target.value
+      if (event.key === 'Enter') {
+        const payload = {
+          ...todo,
+          name: newName,
+        }
+        await this.props.changeTodo(payload, todo)
+        this.props.resetProps()
+      }
+    } catch (error) {
+      this.setState({ errorMessage: error })
+    }
+  }
+
+  onKeyUp = (event) => {
+    const newName = event.target.value
+    this.props.onChangeTyping(newName !== this.props.todo.name)
+  }
+
   render() {
     const {
+      typing,
+      editing,
       todo,
     } = this.props
+
     return (
       <Fragment>
-        { this.state.errorMessage != null ? <AlertPopup type={"danger"} message={this.state.errorMessage} /> : null }
-        <TodosConsumer>
-          {({
-            changeTodo,
-          }) => (
-            <TodoConsumer>
-              {({
-                editing,
-                typing,
-                onChangeTyping,
-                resetProps,
-              }) => (
-                <input
-                  style={{
-                    backgroundColor: getBackgroundColor(typing),
-                    visibility: editing ? 'visible' : 'hidden',
-                    position: editing ? 'relative' : 'absolute',
-                    width: '200px',
-                  }}
-                  defaultValue={todo.name}
-                  onKeyDown={async (event) => {
-                    try {
-                      const newName = event.target.value
-                      if (event.key === 'Enter') {
-                        const payload = {
-                          ...todo,
-                          name: newName,
-                        }
-                        await changeTodo(payload, todo)
-                        resetProps()
-                      }
-                    } catch (error) {
-                      this.setState({ errorMessage: error })
-                    }
-                  }}
-                  onKeyUp={(event) => {
-                    const newName = event.target.value
-                    onChangeTyping(newName !== todo.name)
-                  }}
-                />
-              )}
-            </TodoConsumer>
-          )}
-        </TodosConsumer>
+        { this.state.errorMessage != null ? <AlertPopup type="danger" message={this.state.errorMessage} /> : null }
+        <input
+          style={{
+            backgroundColor: typing ? 'red' : 'white',
+            visibility: editing ? 'visible' : 'hidden',
+            position: editing ? 'relative' : 'absolute',
+            width: '200px',
+          }}
+          defaultValue={todo.name}
+          onKeyDown={this.onKeyDown}
+          onKeyUp={this.onKeyUp}
+        />
       </Fragment>
     )
   }
 }
 
-export default TodoTextInput
+const TodoTextInputContainer = props => (
+  <Compose components={[TodosConsumer, TodoConsumer]}>
+    {({ changeTodo }, actions) => (
+      <TodoTextInput
+        changeTodo={changeTodo}
+        {...actions}
+        {...props}
+      />
+    )}
+  </Compose>
+)
+
+export default TodoTextInputContainer
