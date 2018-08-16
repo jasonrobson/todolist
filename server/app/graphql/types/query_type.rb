dff69c0ce5730a4790fa2298f6ed8769d957cacd
Jaison Robson Gusava <jaison.gusava@gett.com.br>
2018-08-16 17:27:35 -0300

    refact(graphql): removido comentarios desnecessarios

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -1,7 +1,6 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
+
   field :searchTodo, function: Resolvers::SearchTodo.new
   field :allTodos, function: Resolvers::SearchAllTodo.new
 end
