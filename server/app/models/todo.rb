dd59f352dc776706b967d872e4645d051d331285
Jaison Robson Gusava <jaison.gusava@gett.com.br>
2018-08-17 10:45:57 -0300

    refact(server): removido metodo desnecessario

--- server/app/models/todo.rb
+++ server/app/models/todo.rb
@@ -1,9 +1,4 @@
 class Todo < ApplicationRecord
   validates :name, presence: true
   validates :completed, :inclusion => { :in => [false, true]}
-
-  def self.nil_todo
-    Todo.new(name: '', completed: false)
-  end
-
 end
